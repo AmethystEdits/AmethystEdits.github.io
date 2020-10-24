25259ee456cdfb6ddde0c14ad9f095243d38c600
AmethystEdits <jetgamz@gmail.com>
2020-10-25 00:59:53 +1300

    Update hytools ep modu for profit estis and base for gtf made

--- ModulesNStuff/HypixelSkyblockModules/AmethystEngineBFModule.js
+++ ModulesNStuff/HypixelSkyblockModules/AmethystEngineBFModule.js
@@ -1,36 +1,39 @@
 //init area
 let PlayerAPIKey = "";
 let PlayerMoney = 2000;
+let CoopTaxRate = 1.5;
 /*    begin fucntion area     */
 
-/* golden tooth function: (MoneyAvailable(How much money target player is in possetion of),
+/* golden Tooth function: (MoneyAvailable(How much money target player is in possetion of),
 EnchGoldPrice(Price of enchanted gold),NormGoldPrice(Price of regular gold),
 PlayerAPIKey(Api key of target player),WolfToothPrice(Price of normal wolf teeth)) */
-/*function GoldtoothFlipper(MoneyAvailable,EnchGoldPrice,NormGoldPrice,PlayerAPIKey,WolfToothPrice){
+/*function GoldtoothFlipper(MoneyAvailable,EnchGoldPrice,NormGoldPrice,PlayerAPIKey,WolfToothPrice){*/
+function GoldToothFlipper(PriceOfEnchGold,PriceOfGold,PriceOfWolfTeeth,MoneyAvailable){
 
-}*/
+}
+
 /* Enderpearl flipperdoo: (PriceOfEPearls(Price of normal encanted ender pearls),
 PriceOfEnchantedEPearls(price of ehcanted ender pearls),MoneyAvailable(How much money target player is in posseion of),
 PlayerAPIKey(target player's api key)) \\ps ench e pearls sell for 200 at npc */
 function EPearlFlipper(PriceOfEPearls,PriceOfEnchantedEPearls,MoneyAvailable){ //add playerapikey to used things
-    MoneyAvailable = MoneyAvailable / 1.5%;
     let EPearlStatus1 = "";
-    let EpearlsProcessed = Math.abs((PriceOfEPearls*20) - 200)
-    let EEpearlsProcessed = Math.abs(PriceOfEnchantedEPearls - 200)
-    if (EpearlsProcessed>EEpearlsProcessed){
-        if ((MoneyAvailable/EEpearlsProcessed)>71680){
-            var quotient = Math.floor((MoneyAvailable/EEpearlsProcessed)/71680);
-            var remainder = Math.floor(MoneyAvailable/EEpearlsProcessed % 71680);
-            console.log("Normal ender pearls are best at this time, buy " + quotient + " buy order/s and " + remainder + " items for the last order.");
+    let EpearlsFinalCalc = Math.floor(((CoopTaxRate/100)*PriceOfEPearls)+(Math.abs((PriceOfEPearls*20) - 200)));
+    let EEpearlsFinalCalc = Math.floor(((CoopTaxRate/100)*PriceOfEnchantedEPearls)+(Math.abs(PriceOfEnchantedEPearls - 200)));
+    if (EpearlsFinalCalc>EEpearlsFinalCalc){
+        let Profit = Math.floor((Math.abs((PriceOfEPearls*20) - 200))*(MoneyAvailable/EpearlsFinalCalc));
+        if ((MoneyAvailable/EpearlsFinalCalc)>71680){
+            let quotient = (MoneyAvailable/EpearlsFinalCalc)/71680;
+            let remainder = (MoneyAvailable/EpearlsFinalCalc) % 71680;
+            console.log("Normal ender pearls are best at this time, buy " + quotient + " buy order/s and " + remainder + " items for the last order for a estimated " + Profit + " coins of profit.");
         }
         else{
-            console.log("Normal ender pearls crafted into enchanted ender pearls and sold to the npc are best right now, buy " + Math.floor(MoneyAvailable/PriceOfEPearls) + ".")
+            console.log("Normal ender pearls crafted into enchanted ender pearls and sold to the npc are best right now, buy " + Math.floor(MoneyAvailable/EpearlsFinalCalc) +  "for a estimated " + Profit + " coins of profit.")
         }
-        let EPearlStatus1 = "EEPearl_superior";
+        EPearlStatus1 = "EEPearl_superior";
     }
     else{
-        console.log("Enchanted Ender Pearls have a higher margin than an Enchanted ender pearl from 20 normal enderpearls at this time, buy " + Math.floor(MoneyAvailable/PriceOfEnchantedEPearls) + ".")
-        let EPearlStatus1 = "EPearl_superior";
+        console.log("Enchanted Ender Pearls have a higher margin than an Enchanted ender pearl from 20 normal enderpearls at this time, buy " + (MoneyAvailable/EEpearlsFinalCalc) + ".")
+        EPearlStatus1 = "EPearl_superior";
     }
 }
 // use this ""var difference = function (a, b) { return Math.abs(a - b); }"" and get this working properly as this works off of highest cost and not lowest cost, use the number generated and difference it by 200 then sort by lowsest for the answer and round the end number for no decimals//done// and if in eccess of 71680 say 1 full order and (excess stuff)//done// add player api key usage to get the page to respond when youve been outbid by someone else on the bazaar and the exact price and orders neccisary for each of the prics to 7.5 and up to 10
