3b3a1a0e86c69d54c7fd2768c0df15463741aa23
AmethystEdits <jetgamz@gmail.com>
2020-10-27 20:06:09 +1300

    EPF is at v0.9

--- ModulesNStuff/HypixelSkyblockModules/AmethystEngineBFModule.js
+++ ModulesNStuff/HypixelSkyblockModules/AmethystEngineBFModule.js
@@ -18,25 +18,31 @@ PriceOfEnchantedEPearls(price of ehcanted ender pearls),MoneyAvailable(How much 
 PlayerAPIKey(target player's api key)) \\ps ench e pearls sell for 200 at npc */
 function EPearlFlipper(PriceOfEPearls,PriceOfEnchantedEPearls,MoneyAvailable){ //add playerapikey to used things
     let EPearlStatus1 = "";
-    let EpearlsPreCalc = Math.floor(((CoopTaxRate/100)*PriceOfEPearls)+(Math.abs(PriceOfEPearls - 200)));
-    let EpearlsFinalCalc = Math.floor(((CoopTaxRate/100)*PriceOfEPearls)+(Math.abs((PriceOfEPearls*20) - 200)));
-    let EEpearlsFinalCalc = Math.floor(((CoopTaxRate/100)*PriceOfEnchantedEPearls)+(Math.abs(PriceOfEnchantedEPearls - 200)));
-    if (EpearlsFinalCalc>EEpearlsFinalCalc){
-        let Profit = (Math.abs(EpearlsFinalCalc-200))*(MoneyAvailable/EpearlsFinalCalc);
-        if ((MoneyAvailable/EpearlsFinalCalc)>71680){
-            let quotient = Math.floor((MoneyAvailable/EpearlsPreCalc)/71680);
-            let remainder = Math.floor((MoneyAvailable/EpearlsPreCalc) % 71680);
-            console.log("Normal ender pearls are best at this time, buy " + quotient + " buy order/s and " + remainder + " items for the last order for a estimated " + Profit + " coins of profit.");
+    PriceOfEPearls = PriceOfEPearls+(CoopTaxRate/100)*PriceOfEPearls;
+    PriceOfEnchantedEPearls = PriceOfEnchantedEPearls+(CoopTaxRate/100)*PriceOfEnchantedEPearls; // incorporate tax in the final calc
+    let EPPX20 /*e pearl price times 20 */ = PriceOfEPearls*20;
+    if (EPPX20<PriceOfEnchantedEPearls){
+        let quotient = Math.floor((MoneyAvailable/PriceOfEPearls)/71680);
+        let remainder = Math.floor((MoneyAvailable/PriceOfEPearls) % 71680);
+        let Profit = Math.floor((Math.floor(Math.abs(PriceOfEPearls-10)))*(MoneyAvailable/PriceOfEPearls));
+        if ((MoneyAvailable/PriceOfEPearls)>71680){
+            console.log("Normal ender pearls are best at this time, buy " + quotient + " buy order/s of 71680 and " + remainder + " items for the last order for a estimated " + Profit + " coins of profit.");
         }
         else{
-            console.log("Normal ender pearls crafted into enchanted ender pearls and sold to the npc are best right now, buy " + Math.floor(MoneyAvailable/EpearlsFinalCalc) +  "for a estimated " + Profit + " coins of profit.")
+            console.log("Normal ender pearls crafted into enchanted ender pearls and sold to the npc are best right now, buy " + Math.floor(MoneyAvailable/PriceOfEPearls) +  "for a estimated " + Profit + " coins of profit.")
         }
         EPearlStatus1 = "EEPearl_superior";
     }
     else{
-        console.log("Enchanted Ender Pearls have a higher margin than an Enchanted ender pearl from 20 normal enderpearls at this time, buy " + (MoneyAvailable/EEpearlsFinalCalc) + ".")
+        console.log("Enchanted Ender Pearls have a higher margin than an Enchanted ender pearl from 20 normal enderpearls at this time, buy " + (MoneyAvailable/PriceOfEnchantedEPearls) + ".")
         EPearlStatus1 = "EPearl_superior";
     }
-}
+} // Func "EPearlFlipper" is at v0.9,v1 will occor when automatically taking the targetplayer's money and tax rate and detecting and telling the website to say that its happened. this is the final version for manual
 // use this ""var difference = function (a, b) { return Math.abs(a - b); }"" and get this working properly as this works off of highest cost and not lowest cost, use the number generated and difference it by 200 then sort by lowsest for the answer and round the end number for no decimals//done// and if in eccess of 71680 say 1 full order and (excess stuff)//done// add player api key usage to get the page to respond when youve been outbid by someone else on the bazaar and the exact price and orders neccisary for each of the prics to 7.5 and up to 10
-console.warn("AE Build V0.8.9.2 loaded");
+console.warn("AE\\HytoolsModule Build V0.8.9.2 loaded");
+/*exp/copypasta zone
+               num1                   num2
+(Math.abs(PriceOfEnchantedEPearls - 200)) // find difference of
+orientation of num1 and num2 dosent matter
+---   ---   ---   ---   ---
+*/
