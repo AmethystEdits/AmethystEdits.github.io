5a4daa320a8381295d60fb2a76803c486d4a296a
AmethystEdits <jetgamz@gmail.com>
2020-11-22 21:58:36 +1300

    Add fonts, update hytools
    Added Fonts:
    Ubuntu
    Raleway
    HyTools Updates:
        JS:Makes the inner html thingy for buildpinger work
        CSS: added coloring to buildpinger fail text
        HTML:added buidpinger text, made it work, move the <script> tag work now
    AE\\HytoolsModule Build V0.8.9.4 Concluded

--- ModulesNStuff/HypixelSkyblockModules/AmethystEngineBFModule.js
+++ ModulesNStuff/HypixelSkyblockModules/AmethystEngineBFModule.js
@@ -3,7 +3,7 @@ let PlayerAPIKey = "";
 let PlayerMoney = 2000;
 let IGN = "AngxlsInTheSkyy";
 let CoopTaxRate = 1.5;
-let BuildNumber = "AE\\HytoolsModule Build V0.8.9.3 loaded";
+let BuildNumber = "AE\\HytoolsModule Build V0.8.9.4 loaded";
 /*    begin fucntion area     */
 
 /* golden Tooth function: (MoneyAvailable(How much money target player is in possetion of),
@@ -54,6 +54,7 @@ function EPearlFlipper(PriceOfEPearls,PriceOfEnchantedEPearls,MoneyAvailable){ /
 } // Func "EPearlFlipper" is at v0.9,v1 will occor when automatically taking the targetplayer's money and tax rate and detecting and telling the website to say that its happened. this is the final version for manual
 // use this ""var difference = function (a, b) { return Math.abs(a - b); }"" and get this working properly as this works off of highest cost and not lowest cost, use the number generated and difference it by 200 then sort by lowsest for the answer and round the end number for no decimals//done// and if in eccess of 71680 say 1 full order and (excess stuff)//done// add player api key usage to get the page to respond when youve been outbid by someone else on the bazaar and the exact price and orders neccisary for each of the prics to 7.5 and up to 10
 console.warn(BuildNumber);
+document.getElementById("BuildPinger").innerHTML=BuildNumber;
 /*exp/copypasta zone
                num1                   num2
 (Math.abs(PriceOfEnchantedEPearls - 200)) // find difference of
